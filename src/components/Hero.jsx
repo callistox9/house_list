c14d5293324b7c5cf6004632f3d6b6edf7d72aa6
callistox9 <codecallisto@gmail.com>
2023-09-27 03:20:38 +0530

    hero-section till images

---
+++ src/components/Hero.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+export default function Hero()
+{
+    return(
+       
+            <section className="hero-section">
+            <img className="photo-grid"src="src/images/airbnb_photo_grid(1).png"></img>
+            <div className="hero-text-div">
+                <h1 className="here-title">
+                Online Experiences
+                </h1>
+                <p className="hero-para">Join unique interactive activities led
+                    one-of-a-kind hosts-all without leaving home.
+                </p>
+            </div>
+            
+            </section>
+
+    )
+
+}
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+export default function NavBar()
+{
+    return(
+    
+       
+            <nav>
+                <img className="logo_image"src="/src/images/Airbnb-Present-Logo-Since-2014.jpg"></img>
+            </nav>
+        
+    )
+}
