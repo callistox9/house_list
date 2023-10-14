4b47bd92e86364c1d168b9b58b42fdab992d0ac1
callistox9 <codecallisto@gmail.com>
2023-10-14 11:45:08 +0530

    cards-arrannged

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,14 +2,43 @@ import React from "react";
 import NavBar from "./NavBar";
 import Hero from "./Hero";
 import Card from "./Card"; 
+import data from "./data";
 export default function App()
 {
+
+    const cards =data.map(item=>{
+
+        return (
+            <Card
+            img={item.coverImg}
+            rating={item.stats.rating}
+            reviewCount={item.stats.reviewCount}
+            location={item.location}
+            title={item.title}
+            price={item.price}
+            />
+
+
+
+
+
+        )
+
+
+
+
+
+
+    })
     return(
         <div>
             <NavBar/>
-            
             <Hero/>
-            <Card/>
+           <section className="cards-list" >{cards}
+
+           </section>
+            
+            
 
 
 
